bac068986e9b3d2e060519f9108c2b2402fe8888
Nadim1975 <nadim96@gmail.com>
2018-11-07 17:18:16 -0500

    added print to login

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -4,6 +4,14 @@ from selenium.webdriver.common.by import By
 class LoginPage():
 
     def test_validlogin(self):
+
+        print ('hello')
+
+
+
+c = LoginPage()
+
+c.test_validlogin()
         
 
 
