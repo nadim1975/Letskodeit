f7e4309bde6187b35a30ae0867fcf54a115b8f9d
nhussein <nadim96@gmail.com>
2018-11-09 16:11:58 -0500

    recorded login functionality

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -4,15 +4,37 @@ from selenium.webdriver.common.by import By
 class LoginPage():
 
     def test_validlogin(self):
+        baseURL = "https://learn.letskodeit.com/"
+        driver = webdriver.Firefox()
+        driver.implicitly_wait(3)
+        driver.get(baseURL)
+        driver.maximize_window()
 
-        print ('hello')
+        loginLink = driver.find_element(By.LINK_TEXT,"Login")
+        loginLink.click()
+
+        emailAddress = driver.find_element(By.ID,'user_email')
+        emailAddress.send_keys('test@email.com')
+
+        password = driver.find_element(By.ID,'user_password')
+        password.send_keys('abcabc')
+
+        loginButton = driver.find_element(By.NAME,"commit")
+        loginButton.click()
+
+        loginIcon = driver.find_element(By.XPATH,"//*[@id='navbar']//span[text()='Test User']")
+
+        if loginIcon is not None:
+            print('logged in successfully')
+        else:
+            print ('failed to login')
 
 
 
 c = LoginPage()
-
 c.test_validlogin()
-        
+
+
 
 
 
