7cca010290e05fc6d330974ecd9cab36261f89f5
NHussein <nhussein@wfscorp.com>
2018-11-12 21:43:44 -0500

    adding Logging

--- base/selenium_driver.py
+++ base/selenium_driver.py
@@ -3,9 +3,12 @@ from traceback import print_stack
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import *
+import utilities.custom_logger as cl
+import logging
 
 class SeleniumDriver():
 
+    log= cl.customLogger(logging.DEBUG)
     def __init__(self, driver):
         self.driver = driver
 
@@ -24,7 +27,7 @@ class SeleniumDriver():
         elif locatorType == "link":
             return By.LINK_TEXT
         else:
-            print("Locator type " + locatorType + " not correct/supported")
+            self.log.info("Locator type " + locatorType + " not correct/supported")
         return False
 
     def getElement(self, locator, locatorType="id"):
@@ -32,9 +35,9 @@ class SeleniumDriver():
         try:
             byType = self.getByType(locatorType)
             element = self.driver.find_element(byType, locator)
-            print("Element Found with locator: " + locator +" and locatorType: " + locatorType)
+            self.log.info("Element Found with locator: " + locator +" and locatorType: " + locatorType)
         except:
-            print("Element not found with locator: " + locator +" and locatorType: " + locatorType)
+            self.log.info("Element not found with locator: " + locator +" and locatorType: " + locatorType)
         return element
 
     def getElements(self, locator, locatorType="id"):
@@ -42,30 +45,30 @@ class SeleniumDriver():
         try:
             byType = self.getByType(locatorType)
             elements = self.driver.find_elements(byType, locator)
-            print("Elements Found with locator: " + locator +" and locatorType: " + locatorType)
+            self.log.info("Elements Found with locator: " + locator +" and locatorType: " + locatorType)
         except:
-            print("Elements not found with locator: " + locator +" and locatorType: " + locatorType)
+            self.log.info("Elements not found with locator: " + locator +" and locatorType: " + locatorType)
         return elements
 
     def elementClick(self, locator, locatorType="id"):
         try:
             element = self.getElement(locator, locatorType)
             element.click()
-            print("Clicked on element with locator: " +
+            self.log.info("Clicked on element with locator: " +
                   locator + " locatorType: " + locatorType)
         except:
-            print("Cannot click on the element with locator: " +
+            self.log.info("Cannot click on the element with locator: " +
                   locator + " locatorType: " + locatorType)
-            print_stack()
+            self.log.info_stack()
 
     def sendKeys(self,data, locator, locatorType="id"):
         try:
             element = self.getElement(locator, locatorType)
             element.send_keys(data)
-            print("Sent element with locator: " +
+            self.log.info("Sent element with locator: " +
                   locator + " locatorType: " + locatorType +" data: " +data)
         except:
-            print("Cannot send data on the element with locator: " +
+            self.log.info("Cannot send data on the element with locator: " +
                   locator + " locatorType: " + locatorType +" data: " +data)
             print_stack()
 
@@ -74,26 +77,26 @@ class SeleniumDriver():
         try:
             element = self.getElement(locator,locatorType)
             if element is not None:
-                print("Element Found")
+                self.log.info("Element Found")
                 return True
             else:
-                print("Element not found")
+                self.log.info("Element not found")
                 return False
         except:
-            print("Element not found")
+            self.log.info("Element not found")
             return False
 
     def elementPresenceCheck(self, locator, locatorType="id"):
         try:
             elementList = self.getElements(locator,locatorType)
             if len(elementList) > 0:
-                print("Element Found")
+                self.log.info("Element Found")
                 return True
             else:
-                print("Element not found")
+                self.log.info("Element not found")
                 return False
         except:
-            print("Element not found")
+            self.log.info("Element not found")
             return False
 
     def waitForElement(self, locator, locatorType="id",
@@ -101,7 +104,7 @@ class SeleniumDriver():
         element = None
         try:
             byType = self.getByType(locatorType)
-            print("Waiting for maximum :: " + str(timeout) +
+            self.log.info("Waiting for maximum :: " + str(timeout) +
                   " :: seconds for element to be clickable")
             wait = WebDriverWait(self.driver, 10, poll_frequency=1,
                                  ignored_exceptions=[NoSuchElementException,
@@ -109,8 +112,8 @@ class SeleniumDriver():
                                                      ElementNotSelectableException])
             element = wait.until(EC.element_to_be_clickable((byType,
                                                              "stopFilter_stops-0")))
-            print("Element appeared on the web page")
+            self.log.info("Element appeared on the web page")
         except:
-            print("Element not appeared on the web page")
+            self.log.info("Element not appeared on the web page")
             print_stack()
         return element
--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -1,8 +1,10 @@
-from selenium.webdriver.common.by import By
 from base.selenium_driver import SeleniumDriver
+import utilities.custom_logger as cl
+import logging
 
 
 class LoginPage(SeleniumDriver):
+    log = cl.customLogger(logging.DEBUG)
     # constructor method to pass values
     def __init__(self,driver):
         super().__init__(driver)
@@ -12,16 +14,6 @@ class LoginPage(SeleniumDriver):
     _email_field = 'user_email'
     _password_field = 'user_password'
     _login_button = "commit"
-
-    # Methods to expose elements (
-    # def getLoginLink(self):
-    #     return self.driver.find_element(By.LINK_TEXT,self._login_link)
-    # def getEmailField(self):
-    #     return self.driver.find_element(By.ID,self._email_field)
-    # def getPasswordField(self):
-    #     return self.driver.find_element(By.ID,self._password_field)
-    # def getLoginButton(self):
-    #     return self.driver.find_element(By.NAME,self._login_button)
 
     # Methods to perform actions on elements
     def clickLoginLink(self):
